3293644a02b3b890bf1c21f58d31283c18d1395c
Vasko <vaskocuturilo@rambler.ru`>
2019-08-20 11:51:31 +0300

    Feat : FizzBuzz : Added method getFizzBuzz. Test :  FizzBuzzText : Added automation scripts. Commit at 20/08/2019

---
+++ src/main/java/fizbuzz/FizzBuzz.java
@@ -0,0 +1,21 @@
+package fizbuzz;
+
+import java.io.PrintStream;
+
+public class FizzBuzz {
+
+    public static void getFizzBuzz() {
+        PrintStream printStream = System.out;
+        for (int i = 1; i < 100; i++) {
+            if (i % 15 == 0) {
+                printStream.println(" Fizz Buzz " + i);
+            } else if (i % 3 == 0) {
+                printStream.println(" Fizz " + i);
+            } else if (i % 5 == 0) {
+                printStream.println(" Buzz " + i);
+            } else {
+                printStream.println(" " + i);
+            }
+        }
+    }
+}
---
+++ src/test/java/fizbuzz/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+package fizbuzz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void getFizzBuzz() {
+    }
+}
